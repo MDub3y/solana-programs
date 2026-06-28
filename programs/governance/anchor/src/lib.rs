4204f9251898c26c4ed6a1f580b5e8855e13980f
Awesxme <manasvi.d007@gmail.com>
2026-06-28 17:42:53 +0000

    cast vote anchor

---
+++ programs/governance/anchor/src/lib.rs
@@ -0,0 +1,33 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_2022::Token2022;
+
+declare_id!("GovAnchor1111111111111111111111111111111111");
+
+#[program]
+pub mod governance_anchor {
+    use super::*;
+
+    pub fn cast_vote(ctx: Context<CastVote>, proposal_id: u64, weight: u64) -> Result<()> {
+        msg!(
+            "Vote Confirmed for proposal {}: weight {}",
+            proposal_id,
+            weight
+        );
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(proposal_id: u64)]
+pub struct CastVote<'info> {
+    #[account(mut)]
+    pub voter: Signer<'info>,
+
+    #[account(
+        mut,
+        owner = id() @ ErrorCode::ConstraintOwner
+    )]
+    pub proposal_state: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
